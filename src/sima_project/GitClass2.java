3845e266c46ff81f2a6bcb27c33c963354b36ba1
Max <maximelnyk@gmail.comk>
2023-11-10 11:49:37 -0600

    Max add ons

--- src/sima_project/GitClass2.java
+++ src/sima_project/GitClass2.java
@@ -6,5 +6,6 @@ public class GitClass2 {
 
         System.out.println("This is a class for practice");
         System.out.println("I worked very hard");
+        System.out.println("this is good");
     }
 }
