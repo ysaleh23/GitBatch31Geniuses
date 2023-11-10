9ab97916a468aa852b47dff67294e3e944a53db6
handesguven <handesukranguven@gmail.com>
2023-11-10 08:54:04 -0800

    created TC0023

---
+++ src/File1/TC0023.java
@@ -0,0 +1,10 @@
+package File1;
+
+public class TC0023 {
+
+    public static void main(String[] args) {
+        System.out.println("I'm working on the this Test Case");
+
+        // from Hande Guven
+    }
+}
