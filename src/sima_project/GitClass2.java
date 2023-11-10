14e259fb889c51638ec13e8f2774da74bed0a465
simaayazz <140348930+simaayazz@users.noreply.github.com>
2023-11-10 11:47:22 -0600

    updated "GitClass2.java"

--- src/sima_project/GitClass2.java
+++ src/sima_project/GitClass2.java
@@ -5,5 +5,6 @@ public class GitClass2 {
     public static void main(String[] args) {
 
         System.out.println("This is a class for practice");
+        System.out.println("I worked very hard");
     }
 }
